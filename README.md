9abf71df442bcbc527e0d95cda3c2619d84e7b40
Szymon Wygnanski <szymon@spotme.com>
2015-12-01 11:32:36 +0100

    added selecting by 5 lines

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Move fast in your file by 5 lines up and down using:
 
 - ctrl+up to moveFast.up
 - ctrl+down to moveFast.down
+- ctrl+shift+up selects 5 lines above
+- ctrl+shift+down selected 5 lines below
--- src/move-fast.ts
+++ src/move-fast.ts
@@ -15,4 +15,16 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.commands.executeCommand('cursorDown');
 		}
 	}));
+	
+	context.subscriptions.push(vscode.commands.registerCommand('moveFast.upSelect', () => {
+		for (var i = 0; i < NUM_LINES; i += 1) { 
+			vscode.commands.executeCommand('cursorUpSelect');
+		}
+	}));
+	
+	context.subscriptions.push(vscode.commands.registerCommand('moveFast.downSelect', () => {
+		for (var i = 0; i < NUM_LINES; i += 1) { 
+			vscode.commands.executeCommand('cursorDownSelect');
+		}
+	}));
 }
